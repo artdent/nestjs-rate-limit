c65693002e25e4805b1e54ef259bec6073dd11f7
Andre Callus <andrecalllus@outlook.com>
2020-07-17 22:28:52 +1000

    chore(test): fix broken test

--- lib/__tests__/decorator.spec.ts
+++ lib/__tests__/decorator.spec.ts
@@ -1,4 +1,4 @@
-import * as common from '@nestjs/common';
+import * as SetMetadata from '@nestjs/common/decorators/core/set-metadata.decorator';
 
 import { RATE_LIMITER_TOKEN } from '../rate-limiter.constants';
 import { RateLimit } from '../rate-limiter.decorator';
@@ -6,7 +6,7 @@ import { RateLimit } from '../rate-limiter.decorator';
 describe('decorator', () => {
     it('should call SetMetadata', () => {
         const mockSetMetadata = jest.fn();
-        jest.spyOn(common, 'SetMetadata').mockImplementation(mockSetMetadata);
+        jest.spyOn(SetMetadata, 'SetMetadata').mockImplementation(mockSetMetadata);
         RateLimit({ duration: 60 });
 
         expect(mockSetMetadata).toHaveBeenCalledWith(RATE_LIMITER_TOKEN, { duration: 60 });
